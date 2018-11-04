667debc8ffe44404c102752350dca92c4417fcc4
Max Smuli <shmax25@gmail.com>
2018-11-04 01:10:09 -0700

    Realized verification and creation of preconditions execution

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/ContactModificationTests.java
@@ -2,11 +2,13 @@ package ru.stqa.pft.addressbook.Tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactModificationTests extends TestBase {
 
   @Test
   public void testContactModification() {
+    app.getNavigationHelper().goToGroupPage();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData("Ilon","Mask","imask", "VP", "Tesla Motors", "2100 Siera Dr", "5235524", "132123123", "imask@gmail.com", null), false);
     app.getContactHelper().submitContactModification();
