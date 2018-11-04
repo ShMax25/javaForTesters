473e766b3649628bd52c38b0108a31bdb6223810
Max Smuli <shmax25@gmail.com>
2018-11-04 01:35:43 -0700

    Added method goToHomePage

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -18,4 +18,10 @@ public class NavigationHelper extends HelperBase{
     click(By.linkText("groups"));
   }
 
+  public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
+    click(By.linkText("home"));
+  }
 }
