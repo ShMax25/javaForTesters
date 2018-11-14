9419ea848b76f60cca1add7227f396c7da256720
Max Smuli <shmax25@gmail.com>
2018-11-13 21:44:22 -0800

    Comparison of unordered colections

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -14,14 +15,19 @@ public class GroupModificationTests extends TestBase{
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test4","test5", "test6"));
+    GroupData group = new GroupData(before.get(before.size() -1).getId(),"test1","test2", "test3"); // dobavlen id modificirovannoi gruppi
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
 
     Assert.assertEquals(before.size(), after.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after)); // preobrazuem spisok v mnojestva
 
   }
 }
